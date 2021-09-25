9515d15e941eab07c83c432680365236b99dd897
Viraj patel <viraj@toddleapp.com>
2021-09-26 00:48:07 +0530

    fix changes

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 
 var addProduct = require("./routes/addProduct_route");
 var getProduct = require("./routes/getProduct_route");
