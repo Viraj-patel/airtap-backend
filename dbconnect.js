53e8efa782401d2bc0e506045126b56d60e4b67d
Viraj patel <viraj@toddleapp.com>
2021-12-15 19:58:29 +0530

    chore updated database

--- dbconnect.js
+++ dbconnect.js
@@ -1,8 +1,8 @@
 var mysql = require("mysql");
 var connection = mysql.createPool({
-  host: "sql6.freemysqlhosting.net",
-  user: "sql6451012",
-  password: "b5npervpru",
-  database: "sql6451012",
+  host: "sql112.epizy.com",
+  user: "epiz_30599988",
+  password: "Hh4OWiLjdJSve",
+  database: "epiz_30599988_nonsense",
 });
 module.exports = connection;
