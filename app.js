cfe40da50379bee6e635f9c55fa5b5403864d664
Viraj patel <viraj@toddleapp.com>
2023-03-04 15:28:41 +0530

    view

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ var cors = require("cors");
 
 //
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
 app.use(cors());
 
