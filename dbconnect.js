96e8cf7cb6327a4928bdb44588749588ffb0013c
Viraj Patel <62351373+Viraj-patel@users.noreply.github.com>
2024-04-23 14:27:29 +0530

    Update dbconnect.js

--- dbconnect.js
+++ dbconnect.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var connection = mysql.createPool({
-  host: "srv1403.hstgr.io",
+  host: "sql921.main-hosting.eu",
   user: "u647752729_airtap",
   password: "Yash@1234567",
   database: "u647752729_airtap",
